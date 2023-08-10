8bdab40ee6ca13bd618197a0fb1c1f1b58241f09
jose madera <josemadera289@gmail.com>
2023-08-10 14:25:56 -0600

    Configure to library redux toolkit in the App

--- ContratApp/App.tsx
+++ ContratApp/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 import Register from './src/screens/Register/Register';
 import Login from './src/screens/Login/Login';
+import {store} from './src/store/Store';
+import {Provider} from 'react-redux';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <Login></Login>
-      {/* <Register></Register> */}
-    </SafeAreaView>
+    <Provider store={store}>
+      <SafeAreaView style={{flex: 1}}>
+        <Login></Login>
+        {/* <Register></Register> */}
+      </SafeAreaView>
+    </Provider>
   );
 }
 
